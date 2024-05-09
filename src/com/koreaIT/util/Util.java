a46f3ab77df4ad85309777e9b809ecf89251f1c9
CHOMINWOONG <alsdnd0701@naver.com>
2024-05-09 20:12:46 +0900

    article 검색 기능 오류해결

---
+++ src/com/koreaIT/util/Util.java
@@ -0,0 +1,16 @@
+package com.koreaIT.util;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Util {
+	public static String getDateStr() {
+		LocalDateTime now = LocalDateTime.now();
+		
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");       
+        
+        return now.format(formatter);
+	}
+}
+
+
